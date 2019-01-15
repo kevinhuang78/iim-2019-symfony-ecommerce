d69330799bc92543313513d58838a8d7751f5ec4
kevinhuang78 <kevin_huang_iim@yahoo.com>
2019-01-15 11:42:46 +0100

    feat: add fixtures collections

---
+++ src/DataFixtures/CollectionFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Collection;
+use Cocur\Slugify\Slugify;
+use Faker\Factory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CollectionFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // Instancy Slugify
+        $slugify = new Slugify();
+        // Instancy Faker
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 5; $i++){
+            // Tmp var to get same name
+            $name = $faker->word;
+            // Instancy Collection Item Fixtures
+            $collection = new Collection();
+            $collection->setName(ucwords($name));
+            $collection->setSlug($slugify->slugify($name));
+            $collection->setPictureUrl('https://via.placeholder.com/1920x570');
+            $collection->setDateAdd(new \DateTime());
+
+            $manager->persist($collection);
+        }
+
+        $manager->flush();
+    }
+}
