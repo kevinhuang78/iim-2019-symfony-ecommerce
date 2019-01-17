d290e8343bba2bfefaa1bc5eff7437c3550abb5c
kevinhuang78 <kevin_huang_iim@yahoo.com>
2019-01-17 11:56:23 +0100

    feat: productController for each products

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product/{slug}", name="product", methods={"GET"})
+     */
+    public function index($slug)
+    {
+        // Instancy repository and get all products
+        $repositoryProduct = $this->getDoctrine()->getRepository(Product::class);
+        $product = $repositoryProduct->findOneBy([
+            'slug' => $slug
+        ]);
+
+        if (!$product instanceof Product) {
+            throw new NotFoundHttpException('Product not found');
+        }
+
+        return $this->render('product/index.html.twig', [
+            'product' => $product
+        ]);
+    }
+}
