1f737008cb9d0822ccc23a47bd8688503dd45d59
kevinhuang78 <kevin_huang_iim@yahoo.com>
2019-01-26 15:37:35 +0100

    misc: add some fields on menu

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,49 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Address;
-use App\Form\AddressType;
 use App\Form\CardType;
 use App\Model\Card;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @Route("/checkout")
  */
 class CheckoutController extends AbstractController
 {
-    /**
-     * @param UserInterface $user
-     * @param Request $request
-     * @Route("/address", name="checkout_address", methods={"GET", "POST"})
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function deliveryAddress(UserInterface $user, Request $request)
-    {
-        $address = new Address();
-        $form = $this->createForm(AddressType::class, $address);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $address->setUser($user);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($address);
-            $entityManager->flush();
-        }
-
-        $repositoryAddress = $this->getDoctrine()->getRepository(Address::class);
-        $userAddresses = $repositoryAddress->findBy([
-            'user' => $user
-        ]);
-
-        return $this->render('checkout/address.html.twig', [
-            'form' => $form->createView(),
-            'myAddresses' => $userAddresses
-        ]);
-    }
 
     /**
      * @param Request $request
